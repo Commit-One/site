5ccc14fa01eb9b08b57f4eb7e4cb9d15c671c452
Rafael Almeida Gama <rafael.agama@ter.grupomarista.org.br>
2026-04-01 09:41:18 -0300

    fix: style

--- script/index.js
+++ script/index.js
@@ -4,22 +4,23 @@ function changeHeaderScroll() {
 }
 
 // ScrollRevel - configurações do scrollrevel
-const scroll = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 900,
-    delay: 100,
-    easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
-    reset: false,
-    mobile: true
-})
+if (typeof ScrollReveal === 'function') {
+    const scroll = ScrollReveal({
+        origin: 'top',
+        distance: '50px',
+        duration: 900,
+        delay: 100,
+        easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+        reset: false,
+        mobile: true
+    })
 
-scroll.reveal('#home .container > div:first-child, #home .container .image', { origin: 'left', distance: '60px', duration: 1000, delay: 150 })
-scroll.reveal('#about .container > div, #service .wrapper_card', { origin: 'bottom', distance: '40px', duration: 900, interval: 120 })
-scroll.reveal('#partners .container > div', { origin: 'left', distance: '40px', duration: 900, interval: 120 })
-scroll.reveal('#depoiments .wrapper > div', { origin: 'right', distance: '40px', duration: 900, interval: 120 })
-scroll.reveal('#footer .nav, #footer .mobile', { origin: 'bottom', distance: '30px', duration: 800, interval: 60 })
-
+    scroll.reveal('#home .container > div:first-child, #home .container .image', { origin: 'left', distance: '60px', duration: 1000, delay: 150 })
+    scroll.reveal('#about .container > div, #service .wrapper_card', { origin: 'bottom', distance: '40px', duration: 900, interval: 120 })
+    scroll.reveal('#partners .container > div', { origin: 'left', distance: '40px', duration: 900, interval: 120 })
+    scroll.reveal('#depoiments .wrapper > div', { origin: 'right', distance: '40px', duration: 900, interval: 120 })
+    scroll.reveal('#footer .nav, #footer .mobile', { origin: 'bottom', distance: '30px', duration: 800, interval: 60 })
+}
 window.addEventListener('scroll', function () {
     changeHeaderScroll()
 })
